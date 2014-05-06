d4ca81744cc7bcf91b8ae4d954343f003bb0ae76
Austin Ziegler <austin@zieglers.ca>
2014-05-06 01:44:41 -0400

    spec WIP

--- spec/issues_spec.rb
+++ spec/issues_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe "Diff::LCS Issues" do
   include Diff::LCS::SpecHelper::Matchers
 
-  it "should not fail to provide a simple patchset (issue 1)" do
+  it "should not fail to provide a simple patchset (issue 1/string)" do
     s1, s2 = *%W(aX bXaX)
     correct_forward_diff = [
       [ [ '+', 0, 'b' ],
@@ -13,12 +13,59 @@ describe "Diff::LCS Issues" do
     ]
 
     diff_s1_s2 = Diff::LCS.diff(s1, s2)
-    change_diff(correct_forward_diff).should == diff_s1_s2
-    expect do
-      Diff::LCS.patch(s1, diff_s1_s2).should == s2
-    end.to_not raise_error(RuntimeError, /provided patchset/)
-    expect do
-      Diff::LCS.patch(s2, diff_s1_s2).should == s1
-    end.to_not raise_error(RuntimeError, /provided patchset/)
+    expect(change_diff(correct_forward_diff)).to eq(diff_s1_s2)
+
+    expect(Diff::LCS.patch(s1, diff_s1_s2)).to eq(s2)
+    expect(Diff::LCS.patch(s2, diff_s1_s2)).to eq(s1)
   end
+
+  # it "should not fail to provide a simple patchset (issue 1/string)" do
+  #   s2, s1 = *%W(aX bXaX)
+  #   correct_forward_diff = [
+  #     [ [ '-', 0, 'b' ],
+  #       [ '-', 1, 'X' ] ],
+  #   ]
+
+  #   diff_s1_s2 = Diff::LCS.diff(s1, s2)
+  #   expect(change_diff(correct_forward_diff)).to eq(diff_s1_s2)
+
+  #   require 'debugger'
+  #   debugger
+  #   expect(Diff::LCS.patch(s2, diff_s1_s2)).to eq(s1)
+  #   expect(Diff::LCS.patch(s1, diff_s1_s2)).to eq(s2)
+  # end
+
+  # it "should not fail to provide a simple patchset (issue 1/array)" do
+  #   s1 = %w(a X)
+  #   s2 = %W(b X a X)
+  #   correct_forward_diff = [
+  #     [ [ '+', 0, 'b' ],
+  #       [ '+', 1, 'X' ] ],
+  #   ]
+
+  #   diff_s1_s2 = Diff::LCS.diff(s1, s2)
+  #   expect(change_diff(correct_forward_diff)).to eq(diff_s1_s2)
+
+  #   require 'debugger'
+  #   debugger
+  #   expect(Diff::LCS.patch(s2, diff_s1_s2)).to eq(s1)
+  #   expect(Diff::LCS.patch(s1, diff_s1_s2)).to eq(s2)
+  # end
+
+  # it "should not fail to provide a simple patchset (issue 1/array)" do
+  #   s1 = %W(b X a X)
+  #   s2 = %w(a X)
+  #   correct_forward_diff = [
+  #     [ [ '-', 0, 'b' ],
+  #       [ '-', 1, 'X' ] ],
+  #   ]
+
+  #   diff_s1_s2 = Diff::LCS.diff(s1, s2)
+  #   expect(change_diff(correct_forward_diff)).to eq(diff_s1_s2)
+
+  #   require 'debugger'
+  #   debugger
+  #   expect(Diff::LCS.patch(s2, diff_s1_s2)).to eq(s1)
+  #   expect(Diff::LCS.patch(s1, diff_s1_s2)).to eq(s2)
+  # end
 end
